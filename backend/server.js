777c75e5951f1098d13904b5e4e4905a3ed1ce52
OmItaliya7 <omitaa224@gmail.com>
2026-04-02 14:30:50 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors({
   origin: [
@@ -28,7 +29,7 @@ app.get("/", (req, res) => {
 // Routes
 app.use("/contact", contactRoute);
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
