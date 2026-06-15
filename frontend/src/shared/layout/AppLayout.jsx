d95040439b86a25f7c63604a095f6d4c2a29f19b
OmItaliya7 <omitaa224@gmail.com>
2026-06-15 10:10:20 +0530

    scroll issue solve

--- frontend/src/shared/layout/AppLayout.jsx
+++ frontend/src/shared/layout/AppLayout.jsx
@@ -1,5 +1,5 @@
-import { Outlet, useLocation } from "react-router-dom"
-import { useEffect } from "react"
+import { Outlet, ScrollRestoration } from "react-router-dom"
+// import { useEffect } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import { Suspense } from "react"
@@ -7,23 +7,24 @@ import Loader from "../ui/Loader"
 import ErrorBoundary from "../components/ErrorBoundary"
 
 const AppLayout = () => {
-    const location = useLocation();
+    // const location = useLocation();
     
-    useEffect(() => {
-        if (location.hash) {
-            const id = location.hash.replace("#", "");
-            const element = document.getElementById(id);
-            if (element) {
-                element.scrollIntoView({ behavior: "smooth" });
-                return;
-            }
-        }
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    }, [location.pathname, location.hash]);
+    // useEffect(() => {
+    //     if (location.hash) {
+    //         const id = location.hash.replace("#", "");
+    //         const element = document.getElementById(id);
+    //         if (element) {
+    //             element.scrollIntoView({ behavior: "smooth" });
+    //             return;
+    //         }
+    //     }
+    //     window.scrollTo({ top: 0, behavior: "smooth" });
+    // }, [location.pathname, location.hash]);
 
     return (
         <>
             <Header />
+            <ScrollRestoration />
             <main id="main-content" className="flex justify-center">
                 <div className="w-full max-w-[1640px] px-4 sm:px-5 xl:px-20 2xl:px-5.5">
                     <ErrorBoundary>
